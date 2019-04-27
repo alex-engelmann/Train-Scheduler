92e6723a0882d78a2687f4553d7334972a8c6e3e
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-26 17:32:47 -0700

    fully functional no firebase

--- assets/js/trains.js
+++ assets/js/trains.js
@@ -1,5 +1,5 @@
 
-"use strict";
+// "use strict";
 
 //All the trains are kept in an array
 
@@ -9,70 +9,79 @@ var train = new Array();
 train[0] = new Map();
 train[0].set("Name", "Trenton Express");
 train[0].set("Destination", "Trenton");
-train[0].set("Frequency", 30); //This is in minutes
 train[0].set("FirstTime", "00:00");
+train[0].set("Frequency", 30); //This is in minutes
+
 
 train[1] = new Map();
 train[1].set("Name", "Orient Express");
 train[1].set("Destination", "Bucarest");
-train[1].set("Frequency", 120); //This is in minutes
 train[1].set("FirstTime", "06:00");
+train[1].set("Frequency", 120); //This is in minutes
+
 
 train[2] = new Map();
 train[2].set("Name", "Chocolate Choo Choo");
 train[2].set("Destination", "Zurich");
-train[2].set("Frequency", 45); //This is in minutes
 train[2].set("FirstTime", "08:00");
+train[2].set("Frequency", 45); //This is in minutes
+
 
 //This does all the math and updates train time variables for any given train
 var calculateTrain = function (train) {
 
   var tFrequency = train.get("Frequency");
   var firstTime = train.get("FirstTime");
-
-  // First Time (pushed back 1 year to make sure it comes before current time)
   var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-
-  // Current Time
   var currentTime = moment();
-  console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
-
-  // Difference between the times
+  // console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-  // Time apart (remainder)
   var tRemainder = diffTime % tFrequency;
-
-  // Minutes Until Train
   var tMinutesTillTrain = tFrequency - tRemainder;
-  
-  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-  // Next Train
+  // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
- 
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+  // console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
   var arrivalTime = moment(nextTrain).format("HH:mm")
 
-  train.set("MinutesAway", tMinutesTillTrain);
   train.set("NextArrival", arrivalTime);
+  train.set("MinutesAway", tMinutesTillTrain);
 
 }
 
-calculateTrain(train[0]);
-console.log(train[0]);
+var addTrain = function () {
+  var newTrain = new Map();
+  newTrain.set("Name", $("#train-name").val());
+  newTrain.set("Destination", $("#train-destination").val());
+  newTrain.set("FirstTime", $("#train-time").val());
+  newTrain.set("Frequency", $("#train-frequency").val()); //This is in minutes
 
-calculateTrain(train[1]);
-console.log(train[1]);
+  train.push(newTrain);
+}
 
+var clearInterface = function () {
+  //First clear the table
+  $("tbody").empty()
 
+  //Clear input fields
+  $(".form-control").val("");
+}
 
+$(document).on("click", "#submit", function () {
+  addTrain();
+  clearInterface();
+  for (let i = 0; i < train.length; i++) {
+    calculateTrain(train[i]);
+    console.log(train[i]);
+    $("tbody").add("<tr>" +
+      "<th scope='row'>" + train[i].get("Name") + "</th>" +
+      "<td>" + train[i].get("Destination") + "</td>" +
+      "<td>" + train[i].get("Frequency") + "</td>" +
+      "<td>" + train[i].get("NextArrival") + "</td>" +
+      "<td>" + train[i].get("MinutesAway") + "</td>" +
+      "</tr>"
+    ).appendTo("tbody");
+  }
+})
 
-
-
-
-//TODO Get new train from user
-//TODO Add new train to the existing trains
-//TODO Update the table with new train
 
 
