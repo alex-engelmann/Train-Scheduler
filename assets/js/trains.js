a48603bb8ac8b10f16038dc2178bea46e6bef4ec
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-27 00:42:33 -0700

    database not working

--- assets/js/trains.js
+++ assets/js/trains.js
@@ -1,5 +1,5 @@
 
-// "use strict";
+"use strict";
 
 // Initialize Firebase
 var config = {
@@ -14,38 +14,74 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//Configure some starter object trains for the first user
-var myTrains = [
-  {
-    Name: "Trenton Express",
-    Destination: "Trenton",
-    FirstTime: "00:00",
-    Frequency: 30
-  },
-  {
-    Name: "Orient Express",
-    Destination: "Bucarest",
-    FirstTime: "06:00",
-    Frequency: 120
-  },
-  {
-    Name: "Ave",
-    Destination: "Madrid",
-    FirstTime: "08:00",
-    Frequency: 45
-  }
-]
+//Configure some starter object trains for the database.  
+//Was run once and then retired
+
+// var myTrains = [
+//   {
+//     Name: "Trenton Express",
+//     Destination: "Trenton",
+//     FirstTime: "00:00",
+//     Frequency: 30
+//   },
+//   {
+//     Name: "Orient Express",
+//     Destination: "Bucarest",
+//     FirstTime: "06:00",
+//     Frequency: 120
+//   },
+//   {
+//     Name: "Ave",
+//     Destination: "Madrid",
+//     FirstTime: "08:00",
+//     Frequency: 45
+//   }
+// ]
+
+var myTrains = [];
+
+
+//This does all the math and updates train time variables for any given train
+var calculateTrain = function (train) {
+
+  var tFrequency = train.Frequency;
+  var firstTime = train.FirstTime;
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  var currentTime = moment(); //TODO display the current time for the user
+  // console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var tRemainder = diffTime % tFrequency;
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  // console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+  var arrivalTime = moment(nextTrain).format("HH:mm")
+
+  train.NextArrival = arrivalTime;
+  train.MinutesAway = tMinutesTillTrain;
+}
+
+
+for (let i = 0; i < myTrains.length; i++) {
+  calculateTrain(myTrains[i]);
+  console.log("Current Train: " + myTrains[i]);
+
+  $("tbody").add("<tr>" +
+    "<th scope='row'>" + myTrains[i].Name + "</th>" +
+    "<td>" + myTrains[i].Destination + "</td>" +
+    "<td>" + myTrains[i].Frequency + "</td>" +
+    "<td>" + myTrains[i].NextArrival + "</td>" +
+    "<td>" + myTrains[i].MinutesAway + "</td>" +
+    "</tr>"
+  ).appendTo("tbody");
+}
+
 
 // Firebase watcher + initial loader HINT: .on("value")
 database.ref().on("value", function (snapshot) {
 
   // Log everything that's coming out of snapshot
   console.log(snapshot.val().cloudTrains);
-  // console.log(snapshot.val().name);
-  // console.log(snapshot.val().email);
-  // console.log(snapshot.val().age);
-  // console.log(snapshot.val().comment);
-
   // Handle the errors
 }, function (errorObject) {
   console.log("Errors handled: " + errorObject.code);
@@ -102,7 +138,10 @@ $(document).on("click", "#submit", function () {
   addTrain();
   clearInterface();
   for (let i = 0; i < myTrains.length; i++) {
+
+    //Send elsewhere for calculations
     calculateTrain(myTrains[i]);
+
     console.log("Current Train: " + myTrains[i]);
 
     $("tbody").add("<tr>" +
